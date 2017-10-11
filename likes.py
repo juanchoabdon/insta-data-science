86be0719c11e7522e71eb04ad53655f2ff0923b9
Juan Diego Sanchez <juanchoabdon@hotmail.com>
2017-10-11 00:16:31 -0700

    Fix login

--- likes.py
+++ likes.py
@@ -2,11 +2,10 @@ from instapy import InstaPy
 
 session = InstaPy(username='juanchoabdon', password='juanchoabdon1aA', nogui=True) 
 
-
-session.like_by_tags(['#tech', '#js'], amount=100)
-
-
 session.login()
+
+
+session.like_by_feed(amount=100, randomize=True, unfollow=True, interact=True)
 
 
 session.end()
