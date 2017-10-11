36285f929454724c9b193bfcb8b1ab41d70f69b0
Juan Diego Sanchez <juanchoabdon@hotmail.com>
2017-10-10 23:38:00 -0700

    Likes

---
+++ likes.py
@@ -0,0 +1,12 @@
+from instapy import InstaPy
+
+session = InstaPy(username='juanchoabdon', password='juanchoabdon1aA', nogui=True)
+
+
+session.like_by_feed(amount=100, randomize=True, unfollow=True, interact=True)
+
+
+session.login()
+
+
+session.end()
