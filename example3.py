2b2eaccad636df165da29b57f173c3da77c052ac
Daniel Murte <danielmurte_@hotmail.com>
2017-10-11 14:50:09 -0700

    changes

--- example3.py
+++ example3.py
@@ -2,8 +2,10 @@
 from instapy import InstaPy
 session = InstaPy(username='juanchoabdon', password='juanchoabdon1aA')
 session.login()
+
 session.set_do_follow(enabled=True, percentage=10)
 session.like_by_tags(['#tech','#entrepreneurship','#programming'], amount=100)
 session.follow_user_followers(['cvandercito'], amount=500, random=False, sleep_delay= 01)
 session.set_sleep_reduce(01)
+
 session.end()
--- likes.py
+++ likes.py
@@ -1,7 +1,9 @@
 from instapy import InstaPy
 session = InstaPy(username='juanchoabdon', password='juanchoabdon1aA', nogui=True)
 session.login()
+
 session.set_do_follow(enabled=True, percentage=10)
 session.follow_user_followers(['freddiervega'], amount=500, random=False, sleep_delay=01)
 session.set_sleep_reduce(01)
+
 session.end()
